314ec9e042b7d86714e03994321c03d9c41822b2
shatha200 <shathadalhoumy@gmail.com>
2024-08-15 21:46:53 +0100

    tried to fix the ui in sign in/up pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,9 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
   return (
   <div className="bg-tertiary font-sans">
+    <div className="w-full 2 flex items-center justify-center">
     <SignUp />
+    </div>
     </div>
   )
 }
