b0e5791b5b0fe3e1192b2f793791739f68df2737
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2024-08-15 19:40:53 +0300

    Add files via upload

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,44 +1,196 @@
 'use client';
 import React from 'react';
-
-import styles from '../.next/styles/Payment.module.css';
 import Head from 'next/head';
+import { useRouter } from 'next/navigation';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import styles from '../../styles/Home.module.css';
+
 export default function Payment() {
+  const router = useRouter();
+
+  const handleCards = async () => {
+    router.push('/cards');
+  };
+
+  const handlePayment = async () => {
+    router.push('/payment');
+  };
+
+  const handleStart = async () => {
+    router.push('/start');
+  };
   return (
-    <div className={styles.container}>
-        <Head>
-        <title>Payment</title>
-        <meta name="description" content="Payment page" />
-        </Head>
-        
-      <header className={styles.header}>
-        <a href="/" className={styles.logo}>
-          <img src="/images/logo.webp" alt="Logo" />
-        </a>
-        <nav className={styles.navLinks}>
-          <a href="/" className={styles.navLink}>
-            Home
-          </a>
-          <a href="/cards" className={styles.navLink}>
-            Cards
-          </a>
-          <a href="/start" className={styles.navLink}>
-            Start
-          </a>
-          <button className={styles.signInButton}>Sign In</button>
-        </nav>
-      </header>
-
-      <main>
-        <section className={styles.paymentSection}>
-          <h1 className={styles.paymentTitle}>Make a Payment</h1>
-          {/* Add your payment processing code here */}
-        </section>
-      </main>
-
-      <footer className={styles.footer}>
-        {/* Footer content */}
-      </footer>
+    <div className="bg-tertiary font-sans">
+  {/* Header */}
+  <header className="bg-quaternary text-primary border-2 border-primary py-6 px-12 mt-12 mb-12 mx-32 rounded-lg">
+    <div className="container bg-quaternary mx-auto flex justify-between items-center">
+      <div>
+        <img src="/images/logo.webp" alt="Logo" className="h-16" />
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <button 
+            onClick={handleCards}
+            className="text-primary font-bold px-4 py-2 hover:border-b-2 hover:border-primary">
+              Cards
+            </button>
+          </li>
+          <li>
+            <button
+            onClick={handlePayment}
+            className="text-primary font-bold px-4 py-2 hover:border-b-2 hover:border-primary"
+            >
+              Payment
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleStart}
+              className="text-primary font-bold px-4 py-2 hover:border-b-2 hover:border-primary"
+            >
+              Start
+            </button>
+          </li>
+          <li>
+            <button className="bg-primary font-bold text-tertiary border-2 hover:text-primary hover:bg-secondary  px-6 py-3 rounded-lg hover:scale-105">
+              SignUp
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
+  </header>
+
+  <div className="h-[calc(100vh-4rem)] bg-gradient-to-r   px-32">
+      <div className="w-full 2 flex items-center justify-center">
+      <div className="text-center max-w-3xl px-4 md:px-0">
+      <h1 className="text-primary text-3xl sm:text-6xl font-bold text-white mb-8 text-center">
+        The Perfect Plan For Your Needs
+      </h1>
+      <p className="text-senary text-xl text-center mb-8">
+        Our transparent pricing makes it easy to find a plan that works within your financial constraints.
+      </p>
+      <img src="/images/cat-payment.png" alt="Image description" className="m-auto animate-float" />
+      
+    </div>
+  </div>
+  
+      </div>
+
+  {/* Pricing Section */}
+  <section className="px-32 py-12">
+    <h2 className="text-primary text-3xl sm:text-6xl font-bold text-white mb-12 text-center">Choose a Pricing Plan</h2>
+    <div className="grid grid-cols-3 gap-12 mt-8">
+      <div className="bg-gradient-to-tr from-secondary to-tertiary bg-opacity-30 p-12 rounded-[2rem] shadow-lg ">
+        <h3 className="text-primary font-bold text-2xl mb-4">Monthly</h3>
+        <div className="text-4xl font-bold text-primary mb-4">$9</div>
+        <p className="text-primary mb-6">per month</p>
+        <ul className="text-primary mb-8">
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 1
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 2
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 3
+          </li>
+        </ul>
+        <button className="bg-primary font-bold text-tertiary border-2 hover:text-primary hover:bg-secondary px-6 py-3 rounded-lg transition-all duration-300 hover:scale-105">
+        Get Started
+        </button>
+      </div>
+      <div className="bg-gradient-to-br from-secondary to-tertiary bg-opacity-75 p-12 rounded-[2rem] shadow-lg border-2 border-primary">
+        <h3 className="text-primary font-bold text-2xl mb-4">Quarterly</h3>
+        <div className="text-4xl font-bold text-primary mb-4">$24</div>
+        <p className="text-primary mb-6">per quarter</p>
+        <ul className="text-primary mb-8">
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 1
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 2
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 3
+          </li>
+        </ul>
+        <button className="bg-primary font-bold text-tertiary border-2 hover:text-primary hover:bg-secondary px-6 py-3 rounded-lg transition-all duration-300 hover:scale-105">
+          Get Started
+        </button>
+      </div>
+      <div className="bg-gradient-to-tl from-secondary to-tertiary bg-opacity-30 p-12 rounded-[2rem] shadow-lg">
+        <h3 className="text-primary font-bold text-2xl mb-4">Yearly</h3>
+        <div className="text-4xl font-bold text-primary mb-4">$90</div>
+        <p className="text-primary mb-6">per year</p>
+        <ul className="text-primary mb-8">
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 1
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 2
+          </li>
+          <li className="flex items-center mb-2">
+            <svg className="w-6 h-6 mr-2 text-senary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+            Feature 3
+          </li>
+        </ul>
+        <button className="bg-primary font-bold text-tertiary border-2 hover:text-primary hover:bg-secondary px-6 py-3 rounded-lg transition-all duration-300 hover:scale-105">
+        Get Started
+        </button>
+      </div>
+    </div>
+  </section>
+
+  {/* Footer */}
+  <footer className="bg-primary text-white py-6 px-32 ">
+    <div className="flex justify-between items-center mb-4">
+      <div>
+        {/* Logo goes here */}
+        <img src="/images/logo.webp" alt="BRAINDASH" className="h-16 w-full" />
+      </div>
+      <div className="flex justify-between items-center mb-4">
+        {/* Social media icons */}
+        <a href="#" className="text-tertiary hover:text-secondary mr-4">
+          <FaFacebookF className="text-2xl"/>
+        </a>
+        <a href="#" className="text-tertiary hover:text-secondary mr-4">
+          <FaTwitter className="text-2xl"/>
+        </a>
+        <a href="#" className="text-tertiary hover:text-secondary">
+          <FaInstagram className="text-2xl"/>
+        </a>
+      </div>
+    </div>
+    <p className="text-center text-tertiary">© 2024 BRAINDASH. All rights reserved.</p>
+  </footer>
+</div>
   );
 }
