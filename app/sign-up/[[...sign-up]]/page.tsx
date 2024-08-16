cf2ae17f77da310ae4019ab84e9abcf7041c04af
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-08-17 02:01:44 +0530

    centered the form

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,10 +2,10 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-  <div className="bg-tertiary font-sans">
-    <div className="w-full 2 flex items-center justify-center">
-    <SignUp />
-    </div>
+    <div className="bg-tertiary font-sans min-h-screen flex items-center justify-center">
+      <div className="w-full flex items-center justify-center">
+        <SignUp />
+      </div>
     </div>
   )
 }
