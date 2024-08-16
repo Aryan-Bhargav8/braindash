bd969c376c11a96b30113c7ae1b3f2dbd4eb0014
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-08-17 00:12:09 +0530

    fixed eslint errors

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -77,7 +77,7 @@ export default function Start() {
               Introducing our state-of-the-art flashcard app
             </h1>
             <p className="text-senary text-xl text-center mb-8">
-              The ultimate tool to elevate your learning journey. Whether you're a student tackling a challenging course, a professional seeking to expand your expertise, or a lifelong learner driven by curiosity, our platform is designed to empower you every step of the way.
+              The ultimate tool to elevate your learning journey. Whether you&apos;re a student tackling a challenging course, a professional seeking to expand your expertise, or a lifelong learner driven by curiosity, our platform is designed to empower you every step of the way.
             </p>
             <button onClick={handleCards} className="bg-[#F2F4F3] font-bold text-orange border-2 hover:text-tertiary hover:bg-septenary px-6 py-3 rounded-lg hover:scale-105 transition-all duration-300">
               Create Cards
@@ -95,7 +95,7 @@ export default function Start() {
           <div className="bg-orange p-8 rounded-lg">
             <h3 className="text-[#F2F4F3] font-bold text-2xl mb-4">Create Flashcards</h3>
             <p className="text-[#F2F4F3] text-lg mb-8">
-              Start creating your own flashcards by clicking the "Cards" button in the navigation menu. You can add questions, answers, and tags to your cards.
+              Start creating your own flashcards by clicking the &quot;Cards&quot; button in the navigation menu. You can add questions, answers, and tags to your cards.
             </p>
             <button onClick={handleCards} className="bg-[#F2F4F3] font-bold text-orange border-2 hover:text-tertiary hover:bg-septenary px-6 py-3 rounded-lg hover:scale-105 transition-all duration-300">
               Create Cards
@@ -104,7 +104,7 @@ export default function Start() {
           <div className="bg-septenary p-8 rounded-lg">
             <h3 className="text-tertiary font-bold text-2xl mb-4">Manage Payments</h3>
             <p className="text-tertiary text-lg mb-8">
-              To access advanced features, you can upgrade your account by clicking the "Payment" button in the navigation menu.
+              To access advanced features, you can upgrade your account by clicking the &quot;Payment&quot; button in the navigation menu.
             </p>
             <button onClick={handlePayment} className="bg-tertiary font-bold text-septenary border-2 hover:text-tertiary hover:bg-orange px-6 py-3 rounded-lg hover:scale-105 transition-all duration-300">
               Manage Payments
