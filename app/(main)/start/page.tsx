7da8eda4fc274d979861ceeb2aadd0958e340d23
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2024-08-20 12:07:26 +0300

    Update page.tsx

--- app/(main)/start/page.tsx
+++ app/(main)/start/page.tsx
@@ -61,9 +61,11 @@ export default function Start() {
           <div className="relative w-full max-w-[900px] pt-[56.25%]">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://youtu.be/wBylS5jcg3c?si=yScQ-KvuiXdfdO0N"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              src="https://www.youtube.com/embed/s9FWOl4GsSk?si=jrg2p9pfGI2T6lac" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin"
               allowFullScreen
             ></iframe>
           </div>
